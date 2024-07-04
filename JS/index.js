c2ea7cc51676143db389d3a503d8c24449323aaa
Breno Leonel <brenoaleonels@outlook.com>
2024-07-04 20:14:38 -0300

    criando a base da minha evolucao

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+// Pretendo ir atualizando este índice com tudo o que for aprendendo durante o curso, mostrando assim minha evolução. //
+
+const nome = 'Breno Augusto'
+const sobreNome = 'Leonel'
+const idade = 30
+const cidade = 'São José do Rio Preto'
+const estado = 'SP'
+const escola = 'CubosAcademy'
+
+console.log(`Ola, me chamo ${nome} ${sobreNome}, tenho ${idade} anos, sou de ${cidade} - ${estado} e iniciei meu curso em Desenvolvimento de Sistema pela ${escola}.`)
---
+++ TS/index.ts
@@ -0,0 +1,10 @@
+// Pretendo ir atualizando este índice com tudo o que for aprendendo durante o curso, mostrando assim minha evolução. //
+
+const nome: string = 'Breno Augusto'
+const sobreNome: string = 'Leonel'
+const idade: number = 30
+const cidade: string = 'São José do Rio Preto'
+const estado: string = 'SP'
+const escola: string = 'CubosAcademy'
+
+console.log(`Ola, me chamo ${nome} ${sobreNome}, tenho ${idade} anos, sou de ${cidade} - ${estado} e iniciei meu curso em Desenvolvimento de Sistema pela ${escola}.`)
